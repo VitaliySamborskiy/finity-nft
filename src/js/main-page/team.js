c8390164ab819c44cbe7c05987fda44606da7bc0
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-05-16 15:23:52 +0300

    add section team and benefits. add gh-pages branch

---
+++ src/js/main-page/team.js
@@ -0,0 +1,65 @@
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+Swiper.use([Navigation]);
+
+export const teamShow = () => {
+    const buttonClick = document.querySelector(".team__button");
+    const heightBlock = document.querySelector(".team__block");
+    const svgRotate = document.querySelector(".team__svg");
+    const removeClass = document.querySelector(".toggle-class");
+    let swiperInstance = null;
+
+    const initializeSwiper = () => {
+        if (!swiperInstance) {
+            swiperInstance = new Swiper(".swiper", {
+                slidesPerView: "auto",
+                loop: true,
+                navigation: {
+                    nextEl: ".team__button-next",
+                    prevEl: ".team__button-prev",
+                },
+            });
+        }
+    };
+
+    const destroySwiper = () => {
+        if (swiperInstance) {
+            swiperInstance.destroy(true, true);
+            swiperInstance = null;
+        }
+    };
+
+    const handleResize = () => {
+        if (window.matchMedia("(max-width: 1280px)").matches) {
+            if (removeClass.classList.contains("team__block")) {
+                removeClass.classList.remove("team__block");
+            }
+            if (!removeClass.classList.contains("swiper-wrapper")) {
+                removeClass.classList.add("swiper-wrapper");
+            }
+            initializeSwiper();
+        } else {
+            if (!removeClass.classList.contains("team__block")) {
+                removeClass.classList.add("team__block");
+            }
+            if (removeClass.classList.contains("swiper-wrapper")) {
+                removeClass.classList.remove("swiper-wrapper");
+            }
+            destroySwiper();
+        }
+
+        buttonClick.addEventListener("click", function () {
+            removeClass.classList.add("team__block");
+            heightBlock.classList.toggle("team__block_active");
+            svgRotate.classList.toggle("team__svg_active");
+        });
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import { burgerFunction } from "./base/header.js";
+import { teamShow } from "./main-page/team.js";
 
 document.addEventListener("DOMContentLoaded", async function () {
     burgerFunction();
+    teamShow();
 });
