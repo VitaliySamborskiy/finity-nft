6e1f0027ae7944a519e06bdf842c57b9b37bf877
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-05-25 12:45:54 +0300

    add contact-us section rework logic team and acardion section, finalization work on footer

--- src/js/base/faq-section.js
+++ src/js/base/faq-section.js
@@ -1,21 +1,19 @@
 export function acardion() {
-    document.querySelectorAll(".faq__question-border").forEach((el) => {
-        el.addEventListener("click", () => {
-            const fill = el.querySelector(".faq__question-text");
-            const svg = el.querySelector(".faq__question-svg");
-            if (fill.classList.contains("faq__question-text_active")) {
-                fill.classList.remove("faq__question-text_active");
-                svg.classList.remove("faq__question-svg_active");
-            } else {
-                document.querySelectorAll(".faq__question-text").forEach((fill) => {
-                    fill.classList.remove("faq__question-text_active");
-                });
-                document.querySelectorAll(".faq__question-svg").forEach((svgRemove) => {
-                    svgRemove.classList.remove("faq__question-svg_active");
-                });
-                fill.classList.add("faq__question-text_active");
-                svg.classList.add("faq__question-svg_active");
-            }
+    const accordions = document.querySelectorAll(".faq__question-border");
+    accordions.forEach((accordion) => {
+        const title = accordion.querySelector(".faq__question-title-block");
+        const text = accordion.querySelector(".faq__question-text");
+        const svg = accordion.querySelector(".faq__question-svg");
+        title.addEventListener("click", () => {
+            document.querySelectorAll(".faq__question-text").forEach((el) => {
+                el.classList.remove("faq__question-text_active");
+            });
+            document.querySelectorAll(".faq__question-svg").forEach((el) => {
+                el.classList.remove("faq__question-svg_active");
+            });
+
+            text.classList.toggle("faq__question-text_active");
+            svg.classList.toggle("faq__question-svg_active");
         });
     });
 }
--- src/js/main-page/team.js
+++ src/js/main-page/team.js
@@ -3,68 +3,139 @@ import { Navigation } from "swiper/modules";
 
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
+// import "swiper/css/pagination";
 
 Swiper.use([Navigation]);
+
+// export const teamShow = () => {
+//     const buttonClick = document.querySelector(".team__button");
+//     const heightBlock = document.querySelector(".team__block");
+//     const svgRotate = document.querySelector(".team__svg");
+//     const removeClass = document.querySelector(".toggle-class");
+//     let swiperInstance = null;
+//
+//     const initializeSwiper = () => {
+//         if (!swiperInstance) {
+//             swiperInstance = new Swiper(".swiper", {
+//                 slidesPerView: "auto",
+//                 loop: true,
+//                 navigation: {
+//                     nextEl: ".team__button-next",
+//                     prevEl: ".team__button-prev",
+//                 },
+//             });
+//         }
+//     };
+//
+//     const destroySwiper = () => {
+//         if (swiperInstance) {
+//             swiperInstance.destroy(true, true);
+//             swiperInstance = null;
+//         }
+//     };
+//
+//     const handleResize = () => {
+//         if (window.matchMedia("(max-width: 1280px)").matches) {
+//             if (removeClass.classList.contains("team__block")) {
+//                 removeClass.classList.remove("team__block");
+//             }
+//             if (!removeClass.classList.contains("swiper-wrapper")) {
+//                 removeClass.classList.add("swiper-wrapper");
+//             }
+//             initializeSwiper();
+//         } else {
+//             if (!removeClass.classList.contains("team__block")) {
+//                 removeClass.classList.add("team__block");
+//             }
+//             if (removeClass.classList.contains("swiper-wrapper")) {
+//                 removeClass.classList.remove("swiper-wrapper");
+//             }
+//             destroySwiper();
+//         }
+//
+//         buttonClick.addEventListener("click", function () {
+//             removeClass.classList.add("team__block");
+//             heightBlock.classList.toggle("team__block_active");
+//             svgRotate.classList.toggle("team__svg_active");
+//         });
+//     };
+//
+//     handleResize();
+//     window.addEventListener("resize", handleResize);
+// };
+
+// export const teamShow = () => {
+//     let swiper;
+//     swiper = new Swiper(".swiper", {
+//         init: false,
+//         slidesPerView: "auto",
+//         loop: true,
+//         navigation: {
+//             nextEl: ".team__button-next",
+//             prevEl: ".team__button-prev",
+//         },
+//     });
+//
+//     const resizeObserver = () => {
+//         if (window.matchMedia("(max-width: 1280px)").matches) {
+//             return swiper.enable();
+//         } else {
+//             if (swiper !== undefined) {
+//                 swiper.destroy(true, true);
+//                 return swiper;
+//             }
+//         }
+//     };
+//
+//     resizeObserver();
+//     window.addEventListener("resize", resizeObserver);
+// };
+//
+// const buttonClick = document.querySelector(".team__button");
+// const heightBlock = document.querySelector(".team__block");
+// const svgRotate = document.querySelector(".team__svg");
+// buttonClick.addEventListener("click", function () {
+//     heightBlock.classList.toggle("team__block_active");
+//     svgRotate.classList.toggle("team__svg_active");
+// });
 
 export const teamShow = () => {
     const buttonClick = document.querySelector(".team__button");
     const heightBlock = document.querySelector(".team__block");
     const svgRotate = document.querySelector(".team__svg");
-    const removeClass = document.querySelector(".toggle-class");
-    let swiperInstance = null;
+    buttonClick.addEventListener("click", function () {
+        heightBlock.classList.toggle("team__block_active");
+        svgRotate.classList.toggle("team__svg_active");
+    });
 
-    const initializeSwiper = () => {
-        if (!swiperInstance) {
-            swiperInstance = new Swiper(".swiper", {
-                slidesPerView: "auto",
-                loop: true,
-                navigation: {
-                    nextEl: ".team__button-next",
-                    prevEl: ".team__button-prev",
-                },
-            });
-        }
+    const swiperI = (breakpoint, swiperClass, swiperSettings) => {
+        let swiper;
+        breakpoint = window.matchMedia(breakpoint);
+        const swiperInit = (className, settings) => {
+            swiper = new Swiper(className, settings);
+        };
+
+        const checker = () => {
+            if (breakpoint.matches) {
+                return swiperInit(swiperClass, swiperSettings);
+            } else {
+                if (swiper !== undefined) {
+                    return swiper.destroy(true, true);
+                }
+            }
+        };
+
+        breakpoint.addEventListener("change", checker);
+        checker();
     };
 
-    const destroySwiper = () => {
-        if (swiperInstance) {
-            swiperInstance.destroy(true, true);
-            swiperInstance = null;
-        }
-    };
-
-    const handleResize = () => {
-        if (window.matchMedia("(max-width: 1280px)").matches) {
-            if (removeClass.classList.contains("team__block")) {
-                removeClass.classList.remove("team__block");
-            }
-            if (!removeClass.classList.contains("swiper-wrapper")) {
-                removeClass.classList.add("swiper-wrapper");
-            }
-            initializeSwiper();
-        } else {
-            if (!removeClass.classList.contains("team__block")) {
-                removeClass.classList.add("team__block");
-                // if (!removeClass.classList.contains("team__block_active")) {
-                //     heightBlock.classList.remove("team__block_active");
-                // } else {
-                //     heightBlock.classList.add("team__block_active");
-                // }
-            }
-            if (removeClass.classList.contains("swiper-wrapper")) {
-                removeClass.classList.remove("swiper-wrapper");
-            }
-            destroySwiper();
-        }
-
-        buttonClick.addEventListener("click", function () {
-            removeClass.classList.add("team__block");
-            heightBlock.classList.toggle("team__block_active");
-            svgRotate.classList.toggle("team__svg_active");
-        });
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
+    swiperI("(max-width: 1280px)", ".swiper", {
+        slidesPerView: "auto",
+        loop: true,
+        wrapperClass: "team__block",
+        navigation: {
+            nextEl: ".team__button-next",
+            prevEl: ".team__button-prev",
+        },
+    });
 };
