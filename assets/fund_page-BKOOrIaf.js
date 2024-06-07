65392f90d6cf1e9a892893c854667b5e894cd2ad
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-06-07 16:09:45 +0300

    Updates

--- assets/fund_page-BKOOrIaf.js
+++
@@ -1,1 +0,0 @@
-import{b as n,a as o,o as e}from"./observer-video-Dm4HFDNU.js";document.addEventListener("DOMContentLoaded",async function(){n(),o(),e()});
---
+++ assets/fund_page-htUx5z8I.js
@@ -0,0 +1,1 @@
+import{b as n,a as o,o as e}from"./observer-video-Dm4HFDNU.js";document.addEventListener("DOMContentLoaded",async function(){n(),o(),e()});
