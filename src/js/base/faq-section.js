b1b6d9722f8d2e1f07ed424c9451c365bdc7dcc4
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-06-24 19:21:47 +0300

    fixed some bugs

--- src/js/base/faq-section.js
+++ src/js/base/faq-section.js
@@ -4,10 +4,12 @@ export function acardion() {
         const title = accordion.querySelector(".faq__question-title-block");
         const text = accordion.querySelector(".faq__question-text");
         const svg = accordion.querySelector(".faq__question-svg");
+
         title.addEventListener("click", () => {
             document.querySelectorAll(".faq__question-text").forEach((el) => {
                 el.classList.remove("faq__question-text_active");
             });
+
             document.querySelectorAll(".faq__question-svg").forEach((el) => {
                 el.classList.remove("faq__question-svg_active");
             });
