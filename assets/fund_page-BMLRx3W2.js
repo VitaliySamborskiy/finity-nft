0b7a7414038e15fd8d66f061671149dae2757dac
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-06-27 00:50:19 +0300

    Updates

--- assets/fund_page-BMLRx3W2.js
+++
@@ -1,1 +0,0 @@
-import{b as o,d as n}from"./select-DqCnpfuw.js";import{a as r,o as a}from"./observer-video-FFJaoD2G.js";document.addEventListener("DOMContentLoaded",async function(){o(),r(),a(),n()});
---
+++ assets/fund_page-l-54xlI3.js
@@ -0,0 +1,1 @@
+import{b as o,d as n}from"./select-DqCnpfuw.js";import{a as r,o as a}from"./observer-video-CenVYRlq.js";document.addEventListener("DOMContentLoaded",async function(){o(),r(),a(),n()});
