0936fedcf789eedd4cbc7475d0183f2d7844379f
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-06-27 15:20:06 +0300

    Updates

---
+++ assets/fund_page-DlC8KJ-G.js
@@ -0,0 +1,1 @@
+import{b as o,d as n}from"./select-DqCnpfuw.js";import{a as r,o as a}from"./observer-video-CVEW31nc.js";document.addEventListener("DOMContentLoaded",async function(){o(),r(),a(),n()});
--- assets/fund_page-l-54xlI3.js
+++
@@ -1,1 +0,0 @@
-import{b as o,d as n}from"./select-DqCnpfuw.js";import{a as r,o as a}from"./observer-video-CenVYRlq.js";document.addEventListener("DOMContentLoaded",async function(){o(),r(),a(),n()});
